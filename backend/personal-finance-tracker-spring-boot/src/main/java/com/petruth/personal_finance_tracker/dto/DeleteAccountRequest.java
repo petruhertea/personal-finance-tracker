bdf027d0d1298c52a34e111945be31bbc10e924e
Petru <petre.hertea@gmail.com>
2025-11-03 20:53:53 +0200

    backend: - enhanced overall security by implementing refresh token functionality - add scheduling for removing refresh tokens and potential use for budget threshold alarm functionality - add option to allow the user to change their account data, and export their data as JSON,

---
+++ backend/personal-finance-tracker-spring-boot/src/main/java/com/petruth/personal_finance_tracker/dto/DeleteAccountRequest.java
@@ -0,0 +1,19 @@
+package com.petruth.personal_finance_tracker.dto;
+
+public class DeleteAccountRequest {
+    private String password;
+
+    public DeleteAccountRequest() {}
+
+    public DeleteAccountRequest(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
